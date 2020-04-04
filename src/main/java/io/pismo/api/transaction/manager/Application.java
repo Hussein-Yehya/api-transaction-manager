79048c30980e44c33834031925d419d35b20eea1
Hussein Yehya <hussein.ali@becommerce.com.br>
2020-04-04 10:10:26 -0300

    First commit

---
+++ src/main/java/io/pismo/api/transaction/manager/Application.java
@@ -0,0 +1,13 @@
+package io.pismo.api.transaction.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/pismo/api/transaction/manager/ApiTransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pismo.api.transaction.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransactionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
